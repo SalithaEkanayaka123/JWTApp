e8f5addfb291e2034c0150a1b58f4268f5fc523c
Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>
2021-11-03 02:09:50 +0530

    commit 5
    Password Encrypted

--- src/main/java/com/example/jwtapp/config/SecurityConfiguration.java
+++ src/main/java/com/example/jwtapp/config/SecurityConfiguration.java
@@ -1,11 +1,14 @@
 package com.example.jwtapp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -16,11 +19,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         //define username and password for form login authenication
         //here when giving password that password should be encorded
-        auth.inMemoryAuthentication().withUser("Salitha").password("Salitha").authorities("USER","ADMIN");
+        auth.inMemoryAuthentication().withUser("Salitha").password(passwordEncorder().encode("Salitha")).authorities("USER","ADMIN");
 
     }
 
-
+    @Bean
+    public PasswordEncoder passwordEncorder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
