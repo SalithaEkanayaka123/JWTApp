1712aa26bf52051a74da6d182db1d63611c6b16f
Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>
2021-11-02 21:02:31 +0530

    commit 3
    Issue comming in localhost that authentication is needed

--- src/main/java/com/example/jwtapp/config/SecurityConfiguration.java
+++ src/main/java/com/example/jwtapp/config/SecurityConfiguration.java
@@ -19,6 +19,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+       // http.authorizeRequests().anyRequest().permitAll();
+        http.authorizeRequests().anyRequest().authenticated();
     }
 }
