fbf8523d248e026f2cdef71bb5516c74e3925b52
Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>
2021-11-03 05:47:01 +0530

    commit 10
    Implments some codes in User class

--- src/main/java/com/example/jwtapp/entities/User.java
+++ src/main/java/com/example/jwtapp/entities/User.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.persistence.*;
 import java.util.Collection;
+import java.util.List;
 
 @Table(name = "AUTH_USER_DETAILS")
 @Entity
@@ -35,38 +36,42 @@ public class User implements UserDetails {
     @Column(name = "enabled")
     private boolean enabled;
 
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "AUTH_USER_AUTHORITY", joinColumns = @JoinColumn(referencedColumnName = "id"), inverseJoinColumns = @JoinColumn(referencedColumnName = "id"))
+    private List<Authority> authorities;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return this.password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return this.userName;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return this.enabled;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return this.enabled;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return this.enabled;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return this.enabled;
     }
 }
