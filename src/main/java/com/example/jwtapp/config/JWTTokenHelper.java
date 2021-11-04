2c33df9fcd3493fd7108d5545429b1c47d772da7
Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>
2021-11-04 19:26:58 +0530

    commit 17
    JWT helper class implmented

---
+++ src/main/java/com/example/jwtapp/config/JWTTokenHelper.java
@@ -0,0 +1,124 @@
+package com.example.jwtapp.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Date;
+
+@Component
+public class JWTTokenHelper {
+    @Value("$(jwt.auth.app)")
+    private String appName;//which app token has been issued
+
+    @Value("$(jwt.auth.secret_key)")
+    private String secreKey;// useful when you are validating the token
+
+    @Value("$(jwt.auth.expires_in)")
+    private int expiresIn;//expire time
+
+
+    private SignatureAlgorithm SIGNATURE_ALGORITHM = SignatureAlgorithm.HS256;
+
+    private Claims getAllClaimsFromToken (String token){
+        Claims claims;
+        try{
+            claims = Jwts.parser()
+                    .setSigningKey(secreKey)
+                    .parseClaimsJws(token)
+                    .getBody();
+        }catch (Exception e){
+            claims = null;
+        }
+        return claims;
+
+    }
+
+    public String getUsernameFromToken (String token){
+        String username;
+        try{
+            final Claims claims = this.getAllClaimsFromToken(token);
+            username = claims.getSubject();
+        }catch (Exception e){
+            username = null;
+        }
+        return username;
+    }
+
+    public String generateToken (String username) throws InvalidKeySpecException, NoSuchAlgorithmException {
+        return Jwts.builder()
+                .setIssuer(username)
+                .setIssuedAt(new Date())
+                .setExpiration(generateExpirationDate())
+                .signWith(SIGNATURE_ALGORITHM, secreKey)
+                .compact();
+    }
+
+    private Date generateExpirationDate() {
+        return new Date(new Date().getTime() * expiresIn * 1000);
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = getUsernameFromToken(token);
+        return (
+                username != null &&
+                        username.equals(userDetails.getUsername()) &&
+                        !isTokenExpired(token)
+        );
+    }
+
+    public boolean isTokenExpired(String token) {
+        Date expireDate=getExpirationDate(token);
+        return expireDate.before(new Date());
+    }
+
+
+    private Date getExpirationDate(String token) {
+        Date expireDate;
+        try {
+            final Claims claims = this.getAllClaimsFromToken(token);
+            expireDate = claims.getExpiration();
+        } catch (Exception e) {
+            expireDate = null;
+        }
+        return expireDate;
+    }
+
+
+    public Date getIssuedAtDateFromToken(String token) {
+        Date issueAt;
+        try {
+            final Claims claims = this.getAllClaimsFromToken(token);
+            issueAt = claims.getIssuedAt();
+        } catch (Exception e) {
+            issueAt = null;
+        }
+        return issueAt;
+    }
+
+    public String getToken( HttpServletRequest request ) {
+
+        String authHeader = getAuthHeaderFromHeader( request );
+        if ( authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+
+        return null;
+    }
+
+    public String getAuthHeaderFromHeader( HttpServletRequest request ) {
+        return request.getHeader("Authorization");
+    }
+
+}
