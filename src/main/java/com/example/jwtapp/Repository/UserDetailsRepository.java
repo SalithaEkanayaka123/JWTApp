757c6fec66c51a96ef0616840e356e8ac566c10d
Salitha Ekanayaka <68500118+SalithaEkanayaka123@users.noreply.github.com>
2021-11-07 14:03:19 +0530

    commit 22
    Added dependencies needed for mongoDB connection

--- src/main/java/com/example/jwtapp/Repository/UserDetailsRepository.java
+++ src/main/java/com/example/jwtapp/Repository/UserDetailsRepository.java
@@ -3,6 +3,10 @@ package com.example.jwtapp.Repository;
 import com.example.jwtapp.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+//public interface UserDetailsRepository extends JpaRepository<User, Long> {
+//    User findByUserName (String userName);
+//}
+
 public interface UserDetailsRepository extends JpaRepository<User, Long> {
     User findByUserName (String userName);
 }
